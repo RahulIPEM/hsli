0451ac4abc945182c4ed48d301120eff549ec9e2
Sanjeev Kumar <sksanju047@gmail.com>
2020-01-11 17:16:09 +0530

    changes by sanjeev

---
+++ README.md
@@ -0,0 +1,2 @@
+# Hotel Silver Lake Inn
+repository for a hotel's website. Which is located at Bhimtal.
---
+++ js/custom.js
@@ -0,0 +1,20 @@
+/*
+ Template Name: vacayhome
+ File Name: custom.js
+ */
+
+$(document).ready(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('#back-to-top').fadeIn();
+        } else {
+            $('#back-to-top').fadeOut();
+        }
+    });
+    $('#back-to-top').click(function () {
+        $("html, body").animate({scrollTop: 0}, 600);
+        return false;
+    });
+
+});
+
